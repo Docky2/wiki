8bfb09602862e5042d67272cedc7e639be9e54c9
Docky <1606767075@qq.com>
2021-04-14 23:44:53 +0800

    2-2 init

---
+++ src/test/java/com/docky/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docky.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
