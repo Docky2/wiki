1b71b41b0f6a6d880060af8464a3476a1ed152a3
Docky <1606767075@qq.com>
2021-04-15 09:42:53 +0800

    2-5 修改启动文案

--- src/main/java/com/docky/wiki/WikiApplication.java
+++ src/main/java/com/docky/wiki/WikiApplication.java
@@ -1,13 +1,22 @@
 package com.docky.wiki;
 
+
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+/**
+ * @author 16067
+ */
 @SpringBootApplication
 public class WikiApplication {
-
+    private  static final Logger LOG = (Logger) LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        Environment env = SpringApplication.run(WikiApplication.class, args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
