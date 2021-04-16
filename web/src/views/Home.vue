03311eb25049f5656c6ea35b2ba03e43c0234960
Docky <1606767075@qq.com>
2021-04-16 21:38:18 +0800

    5-8 配置axios拦截器打印请求参数和返回参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -98,17 +98,14 @@ for (let i = 0; i < 23; i++) {
 export default defineComponent({
   name: 'Home',
   setup() {
-    console.log("setup");
     const ebooks = ref();  //ref() 函数让其变为一个响应式的数据
     const ebooks1 = reactive({books: []});
+
     onMounted(() => {
-      console.log("onMounted");
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then(
-          (response) => {
+      axios.get("/ebook/list").then((response) => {
             const data = response.data;
             ebooks.value = data.content;
             ebooks1.books = data.content;
-            console.log(response);
           });
     })
     return {
