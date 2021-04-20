e1a3a1933d4a9b1bfc88b28c2aa37cc170bee79f
Docky <1606767075@qq.com>
2021-04-20 19:45:46 +0800

    9-2 用户表设计，并生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -80,3 +80,14 @@ create table `content`(
     primary key (`id`),
     foreign key (`id`) references doc(`id`)
 )engine =innodb default charset=utf8mb4 comment ='文档内容';
+
+
+drop table if exists `user`;
+create table `user`(
+    `id` bigint not null comment 'ID',
+    `username` varchar(50) not null comment '用户名',
+    `name` varchar(50) comment '昵称',
+    `password` char(32) not null comment '密码',
+    primary key (`id`),
+    unique key `login_name_unique` (`username`)
+) engine =innodb default charset = utf8mb4 comment '用户';
